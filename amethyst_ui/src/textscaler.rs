afe9cf53c3c89c73bc6c888e0349ac9fdede3317
p <parimal@beyond8labs.com>
2018-06-25 18:53:35 +1200

    Use ScaleMode inside textresizer

--- amethyst_ui/src/textscaler.rs
+++ amethyst_ui/src/textscaler.rs
@@ -1,13 +1,10 @@
-use amethyst_core::specs::prelude::{Component, DenseVecStorage, Entities, Entity, Join, Read,
-                                    ReadExpect, ReadStorage, Resources, System, Write,
-                                    WriteStorage};
+use amethyst_core::specs::prelude::{Component, DenseVecStorage, Join, ReadExpect, System, WriteStorage};
 use amethyst_renderer::ScreenDimensions;
 use super::*;
 
 pub struct UiTextScaler {
     pub base_size: f32,
-    pub base_width: f32,
-    pub base_height: f32,
+    pub scale_mode: ScaleMode,
 }
 
 impl Component for UiTextScaler {
@@ -18,18 +15,33 @@ impl Component for UiTextScaler {
 pub struct UiTextScalerResources<'s> {
     uitexts: WriteStorage<'s, UiText>,
     scalers: WriteStorage<'s, UiTextScaler>,
-    screendimensions: ReadExpect<'s, ScreenDimensions>,
+    screen_dimensions: ReadExpect<'s, ScreenDimensions>,
 }
 
-pub struct UiTextScalerSystem;
+#[derive(Default)]
+pub struct UiTextScalerSystem {
+    screen_size: (f32, f32),
+}
+
 impl<'s> System<'s> for UiTextScalerSystem {
     type SystemData = UiTextScalerResources<'s>;
 
     fn run(&mut self, mut res: Self::SystemData) {
-        for (text, scaler) in (&mut res.uitexts, &res.scalers).join() {
-            text.font_size = scaler.base_size * 
-                ((res.screendimensions.width() / scaler.base_width) + 
-                (res.screendimensions.height() / scaler.base_height) /2.);
+        let current_screen_size = (res.screen_dimensions.width(), res.screen_dimensions.height());
+        let screen_resized = current_screen_size != self.screen_size;
+        self.screen_size = current_screen_size;
+
+        if screen_resized {
+            for (text, scaler) in (&mut res.uitexts, &res.scalers).join() {
+                text.font_size = match scaler.scale_mode {
+                    ScaleMode::Pixel => {
+                        scaler.base_size
+                    },
+                    ScaleMode::Percent => {
+                        scaler.base_size * res.screen_dimensions.height()
+                    },
+                }
+            }
         }
     }
 }
